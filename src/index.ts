3938322763a8d87cd2e2fb8a5f2a93917ef48897
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-08-07 14:47:31 +0900

    aaa

--- src/index.ts
+++ src/index.ts
@@ -5,70 +5,202 @@ const defaultOptions = {
   attributionPrefix: '---'
 }
 
-function arrayReplaceAt(src: Token[], pos: number, newElements: Token[]) {
-  return ([] as Token[]).concat(
-    src.slice(0, pos),
-    newElements,
-    src.slice(pos + 1)
-  )
-}
-
 function bqcite(md: MarkdownIt, options_?: typeof defaultOptions) {
+  const arrayReplaceAt = md.utils.arrayReplaceAt
   let options = Object.assign({}, defaultOptions)
   options = Object.assign(options, options_)
 
-  md.renderer.rules.blockquote_cite_open = () => '<cite>'
+  md.renderer.rules.blockquote_cite_open = function (
+    tokens,
+    idx,
+    options /*, env */
+  ) {
+    return '<cite>'
+  }
   md.renderer.rules.blockquote_cite_close = () => '</cite>'
 
-  md.core.ruler.push('bqcite', (state) => {
-    let baseLevel = 0
-    let inlineWatcherFlag = false
+  const coreRules = md.core.ruler.getRules('')
+  const inlineRules = md.inline.ruler.getRules('')
+  const blockRules = md.block.ruler.getRules('')
+  console.log(coreRules, inlineRules, blockRules)
+
+  md.core.ruler.before('inline', 'bqcite', (state) => {
+    let bqTokens: Token[] = []
+    let isInBlockquote = false
 
     const tokens = state.tokens
+
     for (let idx = 0; idx < tokens.length; idx++) {
       const token = tokens[idx]
 
-      if (!inlineWatcherFlag) {
-        if (token.type !== 'blockquote_open') {
-          continue
-        } else {
-          inlineWatcherFlag = true
-          continue
-        }
-      } else if (token.type === 'inline') {
-        const children = token.children
+      if (token.type === 'blockquote_open') {
+        isInBlockquote = true
+        continue
+      } else if (token.type === 'blockquote_close') {
+        isInBlockquote = false
+        console.log('#############')
+        console.log('bqTokens:', bqTokens)
+        // bqTokensを処理するfunc
+        makeCiteInBlockquote(bqTokens)
+        bqTokens = []
+        continue
+      }
 
-        if (!children) continue
-
-        for (const [index, childToken] of children.entries()) {
-          if (childToken.content.startsWith(options.attributionPrefix)) {
-            baseLevel = childToken.level
-            const test = singleQuoteLineTokens(childToken.content)
-            token.children = arrayReplaceAt(children, index, test)
-
-            token.content = token.content
-              .replace(options.attributionPrefix, '')
-              .trimStart()
-          }
-        }
+      if (isInBlockquote) {
+        bqTokens = bqTokens.concat(token)
       }
     }
 
-    function singleQuoteLineTokens(quoteLine: string) {
-      const trimmedQuoteLine = quoteLine.trimStart()
+    // md.renderInline(tokens)
 
-      if (trimmedQuoteLine.startsWith(options.attributionPrefix)) {
-        const quoteLineWithoutPrefix = trimmedQuoteLine
-          .replace(options.attributionPrefix, '')
-          .trimStart()
+    /**
+     * @description Blockquote内のTokenを
+     * @param _tokens
+     */
+    function makeCiteInBlockquote(_tokens: Token[]) {
+      // 1個しか見つからない想定
+      const inlineTokens = extractInlineToken(_tokens)
 
+      for (const _inlineToken of inlineTokens) {
+        // atten
+        const orgContent = _inlineToken.content
+        const regexWithWhitespace = new RegExp(
+          options.attributionPrefix + '\\s+',
+          'g'
+        )
+
+        let startFlag = false
+        let followingOne = ''
+        let inCiteTokens: Token[] = []
+        let theOtherTokens: Token[] = []
+
+        if (!_inlineToken.children) continue
+
+        _inlineToken.children.forEach((child) => {
+          if (!startFlag) {
+            // contentが '--- hoge' となる場合とcontent '---'の後のTokenにaタグなどのTokenが続く場合がある。
+            if (child.content.startsWith(options.attributionPrefix)) {
+              startFlag = true
+
+              // attributionPrefixに続く文字があれば切り出し
+              followingOne = child.content.replace(regexWithWhitespace, '')
+              // 内容があれば、citeタグ内のタグとしてカウント
+              if (followingOne) {
+                inCiteTokens = inCiteTokens.concat(child)
+              }
+            } else {
+              theOtherTokens = theOtherTokens.concat(child)
+            }
+          } else {
+            inCiteTokens = inCiteTokens.concat(child)
+          }
+        })
+
+        // '\n'で分割して、---前と---後に分けて処理を行う。---前はテキストとしてciteの外(手前)、---後はciteの中
+        const splitedBqContent = orgContent.split('\n')
+        const beforePrefixContent = splitedBqContent.filter(
+          (c) => !c.startsWith(options.attributionPrefix)
+        )
+        const theOtherInlineToken = inlineToken(
+          theOtherTokens.map((t) => {
+            t.level = _inlineToken.level + 3
+            return t
+          }),
+          beforePrefixContent.join('\n'),
+          _inlineToken.level + 2
+        )
+
+        const inCiteInlineToken = inlineToken(
+          sandwichInCitation(
+            inCiteTokens,
+            followingOne,
+            beforePrefixContent.join('\n'),
+            _inlineToken.level + 3
+          ),
+          followingOne,
+          _inlineToken.level + 2
+        )
+
+        inCiteInlineToken.content = followingOne
+
+        // 新しいchildren(2つ固定)
+        // _inlineToken.children = [theOtherInlineToken, inCiteInlineToken]
+        const parsed = state.md.parse(
+          orgContent.replace(regexWithWhitespace, ''),
+          state.env
+        )
+        console.log('parsed', parsed)
+        _inlineToken.children = parsed.map((t) => {
+          levelUpToken(t, _inlineToken.level)
+          return t
+        })
+        console.log(...parsed)
+
+        _inlineToken.content = orgContent.replace(regexWithWhitespace, '')
+      }
+    }
+    function levelUpToken(_token: Token, level: number) {
+      _token.level = _token.level + level
+      _token.children?.forEach((child) => {
+        levelUpToken(child, level + 1)
+      })
+      return _token
+    }
+
+    /**
+     * @description markdown-it's blockquote must have paragraph_open and paragraph_close. So, extract inline token
+     */
+    function extractInlineToken(_tokens: Token[]) {
+      return _tokens.filter((t) => t.type === 'inline')
+    }
+    /**
+     *
+     * @param _tokens
+     * @param insertWord
+     * @param orgContent
+     * @returns
+     */
+    function sandwichInCitation(
+      _tokens: Token[],
+      insertWord: string,
+      beforeWords: string,
+      level: number
+    ) {
+      // もっとスマートにする
+      if (_tokens.length === 0) return []
+
+      const levelUpTokens: Token[] = _tokens.map((t) => {
+        t.level = t.level + 1
+        return t
+      })
+      // const beforeTokens: Token[] = beforeWords.map((w) =>
+      //   textToken(w, baseLevel)
+      // )
+      const piyo = new state.Token('text', '', 0)
+      piyo.content = 'piyooooooooooo'
+
+      if (insertWord) {
+        const inlineChildren: Token[] = [
+          textToken(insertWord, level + 1),
+          ...levelUpTokens
+        ]
         return [
-          citationOpeningToken(baseLevel + 1),
-          inlineToken(quoteLineWithoutPrefix, baseLevel + 2),
-          citationClosingToken(baseLevel + 1)
+          textToken(beforeWords, level),
+          citationOpeningToken(level),
+          inlineToken(inlineChildren, insertWord, level + 1),
+          citationClosingToken(level),
+          piyo
+        ]
+      } else {
+        const inlineChildren = levelUpTokens
+        return [
+          textToken(beforeWords, level),
+          citationOpeningToken(level),
+          inlineToken(inlineChildren, insertWord, level + 1),
+          citationClosingToken(level),
+          piyo
         ]
       }
-      return [inlineToken(quoteLine, baseLevel)]
     }
 
     function citationOpeningToken(level: number) {
@@ -91,27 +223,23 @@ function bqcite(md: MarkdownIt, options_?: typeof defaultOptions) {
 
       return token
     }
-
-    function paragraphOpeningToken(level: number) {
-      return paragraphToken(level, 1)
-    }
-
-    function paragraphClosingToken(level: number) {
-      return paragraphToken(level, -1)
-    }
-
-    function inlineToken(content: string, level: number) {
+    function textToken(content: string, level: number) {
       const token = new state.Token('text', '', 0)
       token.content = content
       token.level = level
-      token.block = true
+      token.block = false
       return token
     }
-
-    function paragraphToken(level: number, nesting: Token.Nesting) {
-      const token = new state.Token('paragraph_open', 'p', nesting)
+    function inlineToken(
+      children: Token[] | null,
+      content: string,
+      level: number
+    ) {
+      const token = new state.Token(children ? 'inline' : 'text', '', 0)
+      token.content = content
+      token.children = children
       token.level = level
-      token.block = true
+      token.block = !!children
       return token
     }
   })
